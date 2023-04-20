05f691c8d60ec5e455df6bc380f67b750e204263
ross <rostishka1@gmail.com>
2023-04-20 23:39:53 +0300

    Implemented play/pause functionality

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+const playPauseBtn = document.querySelector('.play-pause-btn');
+const videoContainer = document.querySelector('.video-container');
+const video = document.querySelector('video');
+
+
+playPauseBtn?.addEventListener('click', togglePlay)
+
+videoContainer?.addEventListener('click', togglePlay)
+
+video?.addEventListener('play', () => {
+    videoContainer?.classList.remove('paused');
+})
+
+video?.addEventListener('pause', () => {
+    videoContainer?.classList.add('paused');
+})
+
+document.addEventListener('keydown', (e: any) => {
+    switch(e.key.toLowerCase()) {
+        case ' ':
+        case "k":
+            togglePlay()
+            break;
+    }
+})
+
+function togglePlay() {
+    video?.paused ? video.play() : video?.pause()
+}
