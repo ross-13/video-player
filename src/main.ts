266d13e2ac7566b8a9cde3d089af429127e34346
ross <rostishka1@gmail.com>
2023-04-22 00:39:39 +0300

    Implemented full-screen-mode, theater mode, mini player mode

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 const playPauseBtn = document.querySelector('.play-pause-btn');
+const fullScreenBtn = document.querySelector('.full-screen-btn');
+const theatherBtn = document.querySelector('.theater-btn');
+const miniPlayerBtn = document.querySelector('.mini-player-btn');
 const videoContainer = document.querySelector('.video-container');
 const video = document.querySelector('video');
 
 
 playPauseBtn?.addEventListener('click', togglePlay)
+fullScreenBtn?.addEventListener('click', toggleFullScreenMode)
+theatherBtn?.addEventListener('click', toggleTheaterMode)
+miniPlayerBtn?.addEventListener('click', toggleMiniPlayerMode)
 
 video?.addEventListener('click', togglePlay)
 
@@ -21,9 +27,50 @@ document.addEventListener('keydown', (e: any) => {
         case "k":
             togglePlay()
             break;
+        case 'f':
+            toggleFullScreenMode()
+            break;
+        case 'escape':
+            videoContainer?.classList.remove('full-screen');
+            break
+        case 't':
+            toggleTheaterMode()
+            break
     }
 })
 
 function togglePlay() {
     video?.paused ? video.play() : video?.pause()
+}
+
+function toggleFullScreenMode() {
+    if(document.fullscreenElement == null) {
+        videoContainer?.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+}
+
+document.addEventListener('fullscreenchange', () => {
+    videoContainer?.classList.toggle('full-screen', Boolean(document.fullscreenElement));
+})
+
+function toggleMiniPlayerMode() {
+    if(videoContainer?.classList.contains('mini-player')) {
+        document.exitPictureInPicture()
+    } else {
+        video?.requestPictureInPicture()
+    }
+}
+
+document.addEventListener('enterpictureinpicture', () => {
+    videoContainer?.classList.add('mini-player')
+})
+
+document.addEventListener('leavepictureinpicture', () => {
+    videoContainer?.classList.remove('mini-player')
+})
+
+function toggleTheaterMode() {
+    videoContainer?.classList.toggle('theater');
 }
