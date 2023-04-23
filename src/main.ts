43f62de3a324be10d77995ef2b888c258e36b314
ross <rostishka1@gmail.com>
2023-04-23 16:47:06 +0300

    feat: Implemented logic of thumbnail

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,10 @@ const video = document.querySelector('video');
 const currentTimeEl = document.querySelector('.current-time');
 const totalTimeEl = document.querySelector('.total-time');
 const captionsBtn = document.querySelector('.captions-btn');
-const speedBtn = document.querySelector('.speed-btn')
-
+const speedBtn = document.querySelector('.speed-btn');
+const previewImg = document.querySelector('.preview-img');
+const thumbnailImg = document.querySelector('.thumbnail-img');
+const timeLineContainer = document.querySelector('.timeline-container');
 
 playPauseBtn?.addEventListener('click', togglePlay)
 fullScreenBtn?.addEventListener('click', toggleFullScreenMode)
@@ -26,6 +28,9 @@ video?.addEventListener('loadeddata',() => {
 video?.addEventListener('timeupdate', () => {
     // @ts-ignore
     currentTimeEl.textContent = formatTime(video.currentTime)
+    const percent = video.currentTime / video.duration
+    //@ts-ignore
+    timeLineContainer.style.setProperty("--progress-position", percent)
 })
 video?.addEventListener('click', togglePlay)
 video?.addEventListener('volumechange', () => {
@@ -102,6 +107,55 @@ document.addEventListener('enterpictureinpicture', () => {
 document.addEventListener('leavepictureinpicture', () => {
     videoContainer?.classList.remove('mini-player')
 })
+
+//TIMELINE
+
+timeLineContainer?.addEventListener('mousemove', handleTimelineUpdate)
+timeLineContainer?.addEventListener('mousedown', toggleScrubbing)
+document.addEventListener("mouseup", (e) => {
+    if(isScrubbing) toggleScrubbing(e)
+})
+document.addEventListener("mousemove", (e) => {
+    if(isScrubbing) handleTimelineUpdate(e)
+})
+
+let isScrubbing = false;
+let wasPaused = false;
+function toggleScrubbing(e:any) {
+    if(!timeLineContainer || !video) return;
+
+    const rect = timeLineContainer?.getBoundingClientRect()
+    const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+    isScrubbing = (e.buttons & 1) === 1;
+    videoContainer?.classList.toggle('scrubbing', isScrubbing)
+
+    if (isScrubbing) {
+        wasPaused = video.paused;
+        video.pause() 
+    } else {
+        video.currentTime = percent * video.duration
+        if(!wasPaused) video.play();
+    }
+
+    handleTimelineUpdate(e);
+}
+
+function handleTimelineUpdate(e:any) {
+    if(!timeLineContainer || !video) return;
+
+    const rect = timeLineContainer?.getBoundingClientRect()
+    const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+    // const previewImgNumber = Math.max(1, Math.floor((percent * video.duration) / 10))
+    // const previewImg =  TODO: 
+    //@ts-ignore
+    timeLineContainer.style.setProperty("--preview-position", percent)
+
+    if (isScrubbing) {
+        e.preventDefault();
+        //@ts-ignore
+        timeLineContainer.style.setProperty("--progress-position", percent)
+    }
+}
 
 const captions = video!.textTracks[0]
 captions.mode = 'hidden'
